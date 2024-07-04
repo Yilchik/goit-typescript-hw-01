4ec979887bd8dd32e30edd1a5721526c45d3d08a
Yilchik <yulyayulia2@gmail.com>
2024-07-04 09:34:21 +0200

    basic 3

--- src/basic/3.ts
+++ src/basic/3.ts
@@ -1,3 +1,4 @@
-// Створіть змінну, яка може містити або рядок, або число (union type)?
 // Також, оголосіть змінну, яка може містити лише одне з двох можливих рядкових значень:
 // 'enable' або 'disable'(literal type).
+let mixed: string | number;
+let mixedLit: "enable" | "disable";
