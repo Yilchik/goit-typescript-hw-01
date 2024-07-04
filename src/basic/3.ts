b2eb2bb9a1d326ba38646007fa5ebbaa43977133
Yilchik <yulyayulia2@gmail.com>
2024-07-04 21:32:57 +0200

    basic 7

--- src/basic/3.ts
+++ src/basic/3.ts
@@ -1,4 +1,2 @@
-// Також, оголосіть змінну, яка може містити лише одне з двох можливих рядкових значень:
-// 'enable' або 'disable'(literal type).
 let mixed: string | number;
 let mixedLit: "enable" | "disable";
